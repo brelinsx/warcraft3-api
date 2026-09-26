5050b57f4c4277acefed722832432e515e886601
Brelin Ludeña <brelin17@gmail.com>
2026-09-26 18:52:27 +0200

    feat: add warcraft frontend with axios crud and filters

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from backend.database import Base, engine
 from backend import models
@@ -6,6 +7,12 @@ from backend.routers import facciones, heroes
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Warcraft 3 API", version="0.1.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(facciones.router)
 app.include_router(heroes.router)
